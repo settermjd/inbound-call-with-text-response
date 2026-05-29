c6099ce33fa2b08df0a615a6d02bada363cbc2f9
Matthew Setter <matthew@matthewsetter.com>
2026-05-29 10:47:35 +1000

    Add the ability to send SMS in response to incoming SMS
    This change adds the second level of functionality, which is the ability
    to respond to incoming support SMS from the caller, after they have been
    redirected to support.

--- src/Application.php
+++ src/Application.php
@@ -4,14 +4,19 @@ declare(strict_types=1);
 
 namespace App;
 
+use DateTime;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\App as SlimApp;
 use Slim\Interfaces\RouteInterface;
 use Slim\Middleware\ContentLengthMiddleware;
+use Twilio\TwiML\MessagingResponse;
 use Twilio\TwiML\VoiceResponse;
 
+use function is_array;
+use function is_string;
 use function sprintf;
+use function strcasecmp;
 
 /**
  * This class encapsulates the central Slim application, making it easier to
@@ -19,6 +24,28 @@ use function sprintf;
  */
 final class Application
 {
+    public const string OPENING_HOURS          = "Our opening hours are Mon to Fri from 8:30 am to 5:30 pm, and Sat from 9:30 am to 1:30 pm.";
+    public const string SUPPORT_EMAIL          = "For general support, email support@example.org.";
+    public const string ACCOUNT_BILLIING_EMAIL = "For account support, email accounts@example.org. For billing support, email billing@example.org.";
+    public const string POSTAL_ADDRESS         = 'Our mailing address is 1234 Queen Street, Brisbane, QLD, 4000, Australia.';
+    public const string CALLBACK               = 'Thank you for registering for a phone callback. We\'ll call you within the next 30 minutes.';
+    public const string APPOINTMENT            = 'Thank you for seeking an appointment. The next available appointment is at 9:45 am on %s';
+
+    public const array OPTIONS_OPENING_HOURS                 = [
+        'hours',
+        'opening hours',
+    ];
+    public const array OPTIONS_ACCOUNT_BILLING_SUPPORT_EMAIL = [
+        'account support email',
+        'account',
+        'billing support email',
+        'billing',
+    ];
+    public const array OPTIONS_APPOINTMENT                   = [
+        'appointment',
+        'book appointment',
+    ];
+
     public function __construct(private readonly SlimApp $app)
     {
         $app->add(new ContentLengthMiddleware());
@@ -32,7 +59,8 @@ final class Application
      */
     public function setupRoutes(): void
     {
-        $this->app->post('/', [$this, 'handleDefaultRoute']);
+        $this->app->post('/', [$this, 'handleIncomingCall']);
+        $this->app->post('/support', [$this, 'handleSupportRequestsBySms']);
     }
 
     /**
@@ -65,7 +93,7 @@ final class Application
      *
      * @see https://www.twilio.com/docs/voice/twiml#twilios-request-to-your-application
      */
-    public function handleDefaultRoute(
+    public function handleIncomingCall(
         ServerRequestInterface $request,
         ResponseInterface $response,
     ): ResponseInterface {
@@ -77,11 +105,84 @@ final class Application
                 $request->getParsedBody()['From'],
             ),
         );
-        $twimlResponse->redirect(sprintf("%s/support", $_SERVER['BASE_URL']), ["method" => "POST"]);
+        $twimlResponse->redirect(
+            sprintf("%s/support", $_SERVER['BASE_URL']),
+            ["method" => "POST"],
+        );
 
         $response = $response->withHeader("content-type", "application/xml");
         $response->getBody()->write($twimlResponse->asXML());
 
         return $response;
+    }
+
+    /**
+     * Returns TwiML to send a support SMS reply to an incoming SMS
+     *
+     * The body of the response contains TwiML that instructs Twilio to do one of:
+     *
+     *   - Send the opening hours
+     *   - Send the general support email address
+     *   - Send the account and billing support email address
+     *   - Send the business' mailing address
+     *   - Register for a callback
+     *   - Make an appointment
+     *
+     * @see https://www.twilio.com/docs/messaging/twiml/message
+     */
+    public function handleSupportRequestsBySms(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+    ): ResponseInterface {
+        $body    = $request->getParsedBody()['Body'];
+        $message = match (true) {
+            $this->isMatch($body, self::OPTIONS_OPENING_HOURS) => self::OPENING_HOURS,
+            $this->isMatch($body, 'support email') => self::SUPPORT_EMAIL,
+            $this->isMatch($body, self::OPTIONS_ACCOUNT_BILLING_SUPPORT_EMAIL) => self::ACCOUNT_BILLIING_EMAIL,
+            $this->isMatch($body, 'postal address') => self::POSTAL_ADDRESS,
+            $this->isMatch($body, 'callback') => self::CALLBACK,
+            $this->isMatch($body, self::OPTIONS_APPOINTMENT) => sprintf(
+                self::APPOINTMENT,
+                new DateTime('next thursday')->format('l, F jS'),
+            ),
+            default => <<<EOF
+            Please choose from:
+            - Account support email
+            - Billing support email
+            - Book appointment
+            - Callback 
+            - Opening Hours
+            - Postal address
+            - Support Email
+            EOF,
+        };
+
+        $twimlResponse = new MessagingResponse();
+        $twimlResponse->message($message);
+
+        $response = $response->withHeader('content-type', 'application/xml');
+        $response->getBody()->write($twimlResponse->asXML());
+
+        return $response;
+    }
+
+    /**
+     * @param string|array<int,string> $possibleValues
+     */
+    private function isMatch(string $body, string|array $possibleValues): bool
+    {
+        if (is_string($possibleValues)) {
+            return strcasecmp($body, $possibleValues) === 0;
+        }
+
+        if (is_array($possibleValues)) {
+            foreach ($possibleValues as $message) {
+                if (strcasecmp($body, $message) === 0) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
--- tests/Api/HandleIncomingCallCest.php
+++ tests/Api/HandleIncomingCallCest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Tests\Api;
 
+use DateTime;
+use Codeception\Example;
 use Codeception\Util\XmlBuilder;
 use Tests\Support\ApiTester;
 
@@ -21,7 +23,7 @@ final class HandleIncomingCallCest
      * - Respond with a greeting, such as "Thanks for calling, we'll respond via text to answer your questions"
      * - Continue the conversation via SMS (text)
      */
-    public function canReceiveAnIncomingCallAndRespondViaSMS(ApiTester $I): void
+    public function canReceiveAnIncomingCallAndForwardItToSupport(ApiTester $I): void
     {
         $I->haveHttpHeader('Content-Type', 'application/x-www-form-urlencoded');
         $I->sendPost(
@@ -31,6 +33,7 @@ final class HandleIncomingCallCest
                 'Direction' => 'inbound',
             ],
         );
+
         $I->seeResponseCodeIsSuccessful();
         $I->seeResponseIsXml();
         $I->seeHttpHeader("content-type", "application/xml");
@@ -62,5 +65,125 @@ final class HandleIncomingCallCest
                 ->val(sprintf('%s/support', self::BASE_URL))
                 ->attr('method', 'POST');
         $I->seeXmlResponseIncludes($xml->__toString());
+    }
+
+    /**
+     * This test checks whether the support endpoint can respond to the limited
+     * options that it should support.
+     *
+     * The options that are supported are:
+     *   - What are the opening hours? // "1"" or "Opening Hours"
+     *   - What is the general support email address? // "2" or "Support Email"
+     *   - What is the account and billing support email address? // "3" or "Account support email" or "Billing support email"
+     *   - What is the business' mailing address? // "4" or "Mailing address" or "Business mailing address"
+     *   - Register for a callback // "5" or "Callback"
+     *   - Make an appointment to talk about something... // "6" or "Book appointment"
+     */
+    /**
+     * @dataProvider pageProvider
+     */
+    public function canRespondToIncomingSMS(ApiTester $I, Example $example): void
+    {
+        $I->haveHttpHeader('Content-Type', 'application/x-www-form-urlencoded');
+        $I->sendPost(
+            '/support',
+            [
+                'From'      => self::PHONE_NUMBER,
+                'Direction' => 'inbound',
+                'Body'      => $example['body'],
+            ],
+        );
+
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsXml();
+        $I->seeHttpHeader("content-type", "application/xml");
+
+        /**
+         * Example (formatted) response:
+         *
+         * <?xml version="1.0" encoding="UTF-8"?>
+         * <Response>
+         *     <Message>
+         *         Our mailing address is 1234 Queen Street, Brisbane, QLD, 4000, Australia.
+         *     </Message>
+         * </Response>
+         */
+        $xml = new XmlBuilder();
+        $xml->Response
+            ->Message->val($example['response']);
+        $I->seeXmlResponseIncludes($xml->__toString());
+    }
+
+    protected function pageProvider(): array
+    {
+        $openingHoursResponse           = "Our opening hours are Mon to Fri from 8:30 am to 5:30 pm, and Sat from 9:30 am to 1:30 pm.";
+        $supportEmailResponse           = "For general support, email support@example.org.";
+        $accountBillingEmailResponse    = "For account support, email accounts@example.org. For billing support, email billing@example.org.";
+        $businessMailingAddressResponse = "Our mailing address is 1234 Queen Street, Brisbane, QLD, 4000, Australia.";
+        $callbackResponse               = 'Thank you for registering for a phone callback. We\'ll call you within the next 30 minutes.';
+        $appointmentResponse            = sprintf(
+            'Thank you for seeking an appointment. The next available appointment is at 9:45 am on %s',
+            new DateTime('next thursday')->format('l, F jS'),
+        );
+
+        return [
+            [
+                'body'     => 'Opening Hours',
+                'response' => $openingHoursResponse,
+            ],
+            [
+                'body'     => 'Hours',
+                'response' => $openingHoursResponse,
+            ],
+            [
+                'body'     => 'Support Email',
+                'response' => $supportEmailResponse,
+            ],
+            [
+                'body'     => 'Account Support Email',
+                'response' => $accountBillingEmailResponse,
+            ],
+            [
+                'body'     => 'Billing Support Email',
+                'response' => $accountBillingEmailResponse,
+            ],
+            [
+                'body'     => 'Account',
+                'response' => $accountBillingEmailResponse,
+            ],
+            [
+                'body'     => 'Billing',
+                'response' => $accountBillingEmailResponse,
+            ],
+            [
+                'body'     => 'Postal address',
+                'response' => $businessMailingAddressResponse,
+            ],
+            [
+                'body'     => 'Callback',
+                'response' => $callbackResponse,
+            ],
+            [
+                'body'     => 'Book appointment',
+                'response' => $appointmentResponse,
+            ],
+            [
+                'body'     => 'Appointment',
+                'response' => $appointmentResponse,
+            ],
+            [
+                'body'     => 'Something else',
+                'response' => <<<EOF
+                Please choose from:
+                - Account support email
+                - Billing support email
+                - Book appointment
+                - Callback 
+                - Opening Hours
+                - Postal address
+                - Support Email
+                EOF,
+            ],
+        ];
     }
 }
